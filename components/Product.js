388df64bae2541d59ea600cf39497859fc4816ec
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-05 23:54:45 +0330

    complete product list component and show in target page

--- components/Product.js
+++ components/Product.js
@@ -8,16 +8,16 @@ function Product({ item }) {
                 <a>
                     <img src={item.image} alt={item.title} className='rounded-t-xl' />
                 </a>
-                <div className='flex flex-col items-center justify-center p-5'>
-                    <Link href={`/product/${item.slug}`}>
-                        <a>
-                            <h2 className='text-lg'>{item.title}</h2>
-                        </a>
-                    </Link>
-                    <p className='p-2'>{item.price}</p>
-                    <button className='rounded-xl bg-gray-700 text-white px-4 py-2'>Add to cart</button>
-                </div>
             </Link>
+            <div className='flex flex-col items-center justify-center p-5'>
+                <Link href={`/product/${item.slug}`}>
+                    <a>
+                        <h2 className='text-lg'>{item.title}</h2>
+                    </a>
+                </Link>
+                <p className='p-2'>{item.price}</p>
+                <button className='rounded-xl bg-gray-700 text-white px-4 py-2'>Add to cart</button>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Layout from "../components/Layout"
+import Product from "../components/Product"
+import ProductItems from "../data/products.json"
 
 function Home() {
   return (
     <Layout title={"Home page"}>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+      <div className="grid grid-cols-1 gap-12 md:grid-cols-3 lg:grid-cols-4">
+        {ProductItems.map((item) => (
+          <Product item={item} key={item.slug}></Product>
+        ))}
+      </div>
     </Layout>
   )
 }
