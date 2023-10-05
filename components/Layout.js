edc935da2163b197b322f2acce9f69d665f07782
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-05 23:23:27 +0330

    develop header basic

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,32 @@
+import Head from 'next/head'
+import Link from 'next/link'
 import React from 'react'
 
-function Layout(props) {
+function Layout({ children, title }) {
     return (
         <>
-            <header>header</header>
-            <main>{props.children}</main>
-            <footer>footer</footer>
+            <Head>
+                <title>{`${title} - Shopping`}</title>
+            </Head>
+            <div className='flex min-h-screen flex-col justify-between'>
+                <header>
+                    <nav className='flex h-14 px-8 justify-between items-center border-b-4'>
+                        <Link href={"/"}>
+                            <a className='text-lg font-bold'>Shopping</a>
+                        </Link>
+                        <div>
+                            <Link href={"/cart"}>
+                                <a className='p-2'>Cart</a>
+                            </Link>
+                            <Link href={"/login"}>
+                                <a className='p-2'>Login</a>
+                            </Link>
+                        </div>
+                    </nav>
+                </header>
+                <main>{children}</main>
+                <footer>footer</footer>
+            </div>
         </>
     )
 }
