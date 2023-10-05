25a38e566ebbe71f80313916066501e6d5d23bb9
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-05 21:43:16 +0330

    test tailwind classes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className={styles.container}>
-      test
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
