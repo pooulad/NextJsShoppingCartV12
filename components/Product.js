8a11a998867993d7a8bd9eaa78beaad9c8245e8b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-05 23:40:33 +0330

    develop product file

--- components/Product.js
+++ components/Product.js
@@ -1,9 +1,25 @@
+import Link from 'next/link'
 import React from 'react'
 
-function Product() {
-  return (
-    <div>Product</div>
-  )
+function Product({ item }) {
+    return (
+        <div className='bg-white rounded-xl mb-5 block'>
+            <Link href={`/product/${item.slug}`}>
+                <a>
+                    <img src={item.image} alt={item.title} className='rounded-t-xl' />
+                </a>
+                <div className='flex flex-col items-center justify-center p-5'>
+                    <Link href={`/product/${item.slug}`}>
+                        <a>
+                            <h2 className='text-lg'>{item.title}</h2>
+                        </a>
+                    </Link>
+                    <p className='p-2'>{item.price}</p>
+                    <button className='rounded-xl bg-gray-700 text-white px-4 py-2'>Add to cart</button>
+                </div>
+            </Link>
+        </div>
+    )
 }
 
 export default Product
