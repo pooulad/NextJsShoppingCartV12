ea9b17d553d35d4932c22c01dc665b09303c4450
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-05 21:51:46 +0330

    use layout in index file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Layout from "../components/Layout"
+
 function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <Layout>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </Layout>
   )
 }
 
