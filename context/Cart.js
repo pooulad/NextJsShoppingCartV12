2dcc448eff62a5dcb47919cf979269485bd657f4
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-07 01:17:12 +0330

    Update Cart.js

--- context/Cart.js
+++ context/Cart.js
@@ -16,11 +16,13 @@ function reducer(state, action) {
             const existingItem = state.cart.cartItems.find((item) => item.slug === newItem.slug)
             const cartItems = existingItem ? state.cart.cartItems.map((item) => item.title === existingItem.title ? newItem : item) : [...state.cart.cartItems, newItem]
 
+            Cookies.set("cart", JSON.stringify({ ...state.cart, cartItems }))
             return { ...state, cart: { ...state.cart, cartItems } }
         }
         case "REMOVE_ITEM": {
             const cartItems = state.cart.cartItems.filter((item) => item.slug !== action.payload.slug)
 
+            Cookies.set("cart", JSON.stringify({ ...state.cart, cartItems }))
             return { ...state, cart: { ...state.cart, cartItems } }
         }
         default:
