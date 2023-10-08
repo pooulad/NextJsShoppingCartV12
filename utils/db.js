3c78409da802e61f607cdc8203848957a1dfe03a
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-08 19:15:24 +0330

    setup mongoose in db.js file

---
+++ utils/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+async function connect() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/shoppingv12")
+
+    console.log("Connected");
+}
+
+const db = { connect }
+
+export default db
