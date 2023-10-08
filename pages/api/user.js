c374f3da7e783aab7ece2ee83f366862e29933e3
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-08 19:28:46 +0330

    insert users in db

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,3 +1,11 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import db from "../../utils/db"
+import User from "../../models/user"
+import userItems from "../../data/users.json"
+
+export default async function handler(req, res) {
+  await db.connect()
+
+  await User.insertMany(userItems)
+
+  res.send({ message: "user added" })
 }
