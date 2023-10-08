a3fde0ecf0c21ce11b45d7534ea62759635bafea
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-08 21:04:13 +0330

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 
 async function connect() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/shoppingv12")
+    const uri = "mongodb+srv://poooooladi:YNT4q0yU57LqmDlb@cluster0.oc3ohkf.mongodb.net/?retryWrites=true&w=majority";
+
+    await mongoose.connect(uri)
 
     console.log("Connected");
 }
