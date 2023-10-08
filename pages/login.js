4f52e53891aadd01eff8b37e006fa69b9ab0acd3
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-08 18:40:39 +0330

    setup use form in login page

--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import Layout from "../components/Layout"
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
 
 function login() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    function submitHandler({email,password}){
+
+    }
     return (
         <Layout title="Login">
-            <form className='mx-auto max-w-screen-md'>
+            <form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
                 <h2 className='mb-4 text-xl'>Login</h2>
                 <div className='mb-4'>
-                    <input type='email' className='w-full rounded-xl p-2 outline-0' id='email' placeholder='Email' autoFocus />
+                    <input {...register("email",{required : true})} type='email' className='w-full rounded-xl p-2 outline-0' id='email' placeholder='Email' autoFocus />
                 </div>
+                {errors.email && (
+                    <div className='text-red-500'>Please enter email.</div>
+                )}
                 <div className='mb-4'>
-                    <input type='password' className='w-full rounded-xl p-2 outline-0' id='password' placeholder='Password' autoFocus />
+                    <input {...register("password",{required : true,minLength:{
+                        value : 5,
+                        message : "Password must be at least 5 chars."
+                    }})} type='password' className='w-full rounded-xl p-2 outline-0' id='password' placeholder='Password' autoFocus />
                 </div>
+                {errors.password && (
+                    <div className='text-red-500'>{errors.password.message}</div>
+                )}
                 <div className='mb-4'>
                     <button className='rounded-xl bg-gray-700 text-white px-4 py-2 w-28'>Lohin</button>
                 </div>
