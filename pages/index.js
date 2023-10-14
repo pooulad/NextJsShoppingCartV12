f1cbe9e9683e98fc08e7753e99b51d40b89b8d4a
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 02:15:41 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import ProductItem from "../components/ProductItem"
 import db from "../utils/db"
 import Product from "../models/product"
 
-function Home() {
+function Home({products}) {
   return (
     <Layout title={"Home page"}>
       <div className="grid grid-cols-1 gap-12 md:grid-cols-3 lg:grid-cols-4">
-        {ProductItems.map((item) => (
-          <Product item={item} key={item.slug}></Product>
+        {products.map((item) => (
+          <ProductItem item={item} key={item.slug}></ProductItem>
         ))}
       </div>
     </Layout>
