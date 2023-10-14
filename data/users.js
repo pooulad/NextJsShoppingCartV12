82b24b2654e2ac7a3c21b9df549cb181e9383de3
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 01:00:12 +0330

    hash password before save in db

--- data/users.js
+++ data/users.js
@@ -1,14 +1,19 @@
-[
+import bcrypt from "bcryptjs"
+
+const users = [
   {
-    "name": "user 1",
-    "email": "user1@gmail.com",
-    "password": "12345",
-    "isAdmin": true
+    name: "user 1",
+    email: "user1@gmail.com",
+    password: bcrypt.hashSync("12345"),
+    isAdmin: true
   },
   {
-    "name": "user 2",
-    "email": "user2@gmail.com",
-    "password": "54321",
-    "isAdmin": false
+    name: "user 2",
+    email: "user2@gmail.com",
+    password: bcrypt.hashSync("54321"),
+    isAdmin: false
   }
 ]
+
+
+export default users
