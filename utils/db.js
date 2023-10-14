a037a9183727a22a1183d920c3076fe6816fc579
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 00:23:57 +0330

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 
 async function connect() {
-    const uri = "mongodb+srv://poooooladi:YNT4q0yU57LqmDlb@cluster0.oc3ohkf.mongodb.net/?retryWrites=true&w=majority";
+    // online address sample
+    // const uri = "mongodb+srv://poooooladi:YNT4q0yU57LqmDlb@cluster0.oc3ohkf.mongodb.net/?retryWrites=true&w=majority";
 
     await mongoose.connect(uri)
 
