d7ad96fe55ea2f08b8d7e25e52308bee4c83b9be
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 02:03:01 +0330

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    slug: { type: String, required: true },
+    description: { type: String, required: true },
+    cat: { type: String, required: true },
+    cound: { type: Number, required: true, default: 0 },
+    image: { type: String, required: true },
+})
+
+const Product = mongoose.models.Product || mongoose.model('Product', productSchema)
+
+export default Product
