e5e17b572a023c2ea2981a0d46edcf065421985b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 01:32:31 +0330

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -17,8 +17,8 @@ export default NextAuth({
             return token
         },
         async session({ session, token }) {
-            if (token?._id) session.user?._id = token?._id
-            if (token?.isAdmin) session.user?.isAdmin = token?.isAdmin
+            if (token?._id) session.user._id = token?._id
+            if (token?.isAdmin) session.user.isAdmin = token?.isAdmin
 
             return session
         }
