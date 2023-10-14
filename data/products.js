9bb3ad92b52b486e9645cf52806c929e016e7cd5
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 02:02:50 +0330

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    title: "book 1",
+    price: 300,
+    slug: "book-1",
+    description: "test description",
+    cat: "books",
+    cound: 10,
+    image: "/images/book.jpg"
+  },
+  {
+    title: "book 2",
+    price: 200,
+    slug: "book-2",
+    description: "test description",
+    cat: "books",
+    cound: 10,
+    image: "/images/book.jpg"
+  }
+]
+
+
+export default products
