497baac51695941683917eb0a879f9f36febd8dc
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 00:53:59 +0330

    Create users.js

---
+++ data/users.js
@@ -0,0 +1,14 @@
+[
+  {
+    "name": "user 1",
+    "email": "user1@gmail.com",
+    "password": "12345",
+    "isAdmin": true
+  },
+  {
+    "name": "user 2",
+    "email": "user2@gmail.com",
+    "password": "54321",
+    "isAdmin": false
+  }
+]
