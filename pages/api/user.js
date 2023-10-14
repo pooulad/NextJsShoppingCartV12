b6b29e8541a2990a9e27b10307a4b6c7bda4c261
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 01:02:36 +0330

    change path for users in user.js file(api folder)

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,9 +1,11 @@
 import db from "../../utils/db"
 import User from "../../models/user"
-import userItems from "../../data/users.json"
+import userItems from "../../data/users"
 
 export default async function handler(req, res) {
   await db.connect()
+
+  await User.deleteMany()
 
   await User.insertMany(userItems)
 
