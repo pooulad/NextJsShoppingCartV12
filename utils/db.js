c885a1a0c66437da01011158d2513f1f7bd65f15
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 00:24:15 +0330

    add local config for test mongodb in local server

--- utils/db.js
+++ utils/db.js
@@ -5,6 +5,12 @@ async function connect() {
     // online address sample
     // const uri = "mongodb+srv://poooooladi:YNT4q0yU57LqmDlb@cluster0.oc3ohkf.mongodb.net/?retryWrites=true&w=majority";
 
+
+    // connect to local server with run this code :
+    // step 1 : cd "C:\Program Files\MongoDB\Server\[MongoDB version]\bin"
+    // step 2 : mongod -> runs the server in cmd
+    const uri = "";
+
     await mongoose.connect(uri)
 
     console.log("Connected");
