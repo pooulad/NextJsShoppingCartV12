75516e18d3f052ac30b3c40cf290b40e81a0ad66
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 02:23:05 +0330

    handle confilicts in [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -16,40 +16,40 @@ function SingleProductPage({ product }) {
 
     function addToCartHandler() {
         const existingItem = state.cart.cartItems.find(
-            (item) => item.slug === Product.slug
+            (item) => item.slug === product.slug
         )
 
         const qty = existingItem ? existingItem.qty + 1 : 1
 
-        if (Product.cound < qty) {
+        if (product.cound < qty) {
             alert('You cant add this product anymore')
             return
         }
 
-        dispatch({ type: "ADD_ITEMS", payload: { ...Product, qty } })
+        dispatch({ type: "ADD_ITEMS", payload: { ...product, qty } })
 
         router.push("/cart")
     }
     return (
-        <Layout title={Product.title}>
+        <Layout title={product.title}>
             <div className='grid md:grid-cols-4 md:gap-3 bg-white rounded-xl p-10'>
                 <div className='md:col-span-2'>
-                    <Image className='rounded-xl' src={Product.image} width={340} height={340} layout='responsive' />
+                    <Image className='rounded-xl' src={product.image} width={340} height={340} layout='responsive' />
                 </div>
                 <div>
                     <div className='text-lg'>
-                        <h2>{Product.title}</h2>
-                        <p>{Product.cat}</p>
-                        <p>{Product.description}</p>
+                        <h2>{product.title}</h2>
+                        <p>{product.cat}</p>
+                        <p>{product.description}</p>
                     </div>
                     <div className='p-5'>
                         <div className='mb-2 flex justify-between'>
                             <div>Price:</div>
-                            <div>{Product.price}</div>
+                            <div>{product.price}</div>
                         </div>
                         <div className='mb-2 flex justify-between'>
                             <div>Status:</div>
-                            <div>{Product.cound > 0 ? "Available" : "Unavailable"}</div>
+                            <div>{product.cound > 0 ? "Available" : "Unavailable"}</div>
                         </div>
                         <button onClick={addToCartHandler} className='rounded-xl bg-gray-700 text-white px-4 py-2 w-full'>Add to cart</button>
                     </div>
