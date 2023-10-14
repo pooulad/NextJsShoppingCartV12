4d46ad3cdee6687f6bf2b52bb0a0ea35b9e655bb
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 02:02:58 +0330

    Create users.js

---
+++ pages/api/users.js
@@ -0,0 +1,13 @@
+import db from "../../utils/db"
+import User from "../../models/user"
+import userItems from "../../data/users"
+
+export default async function handler(req, res) {
+  await db.connect()
+
+  await User.deleteMany()
+
+  await User.insertMany(userItems)
+
+  res.send({ message: "users added" })
+}
