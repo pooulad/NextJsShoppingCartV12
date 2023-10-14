51c1ddb4cd00768699b9365e38c068cf134656a2
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 01:51:07 +0330

    implement DropDown Item in this component

---
+++ components/DropDown.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+function DropDown({ href, children, ...rest }) {
+    return (
+        <Link href={href}>
+            <a {...rest}>{children}</a>
+        </Link>
+    )
+}
+
+export default DropDown
