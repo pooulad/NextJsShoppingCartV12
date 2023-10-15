e5748f7ba34f509cc69f5124b3e1cb64e84f8c9b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 03:56:05 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import { CartContextProvider } from '../context/Cart'
 import '../styles/globals.css'
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider, useSession } from "next-auth/react"
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <div className='bg-gray-100'>
       <SessionProvider session={session}>
         <CartContextProvider>
-          <Component {...pageProps} />
+          <Auth adminOnly={Component.auth.adminOnly}>
+            <Component {...pageProps} />
+          </Auth>
         </CartContextProvider>
       </SessionProvider>
     </div>
