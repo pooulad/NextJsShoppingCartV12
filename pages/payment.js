a190b0d4107aed754c5bd36174bd014580a14187
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 04:06:21 +0330

    Update payment.js

--- pages/payment.js
+++ pages/payment.js
@@ -37,3 +37,41 @@ function PaymentPage() {
 
     const methods = ['Gateway', 'Offline Payment']
 
+    return (
+        <Layout title='Payment Page'>
+            <CheckoutWizard activeStep={2} />
+            <form className='mx-auto max-w-screen-md' onSubmit={submitHandler}>
+                <h2 className='mb-4 text-xl'>Payment Method</h2>
+                {methods.map((item) => (
+                    <div key={item} className='mb-4'>
+                        <input
+                            name='paymentMethod'
+                            className='p-2 outline-none focus:ring-0'
+                            id={item}
+                            type='radio'
+                            checked={selectedPaymentMethod === item}
+                            onChange={() => setSelectedPaymentMethod(item)}
+                        />
+                        <label className='p-2' htmlFor={item}>
+                            {item}
+                        </label>
+                    </div>
+                ))}
+                <div className='mb-4 flex justify-between'>
+                    <button
+                        onClick={() => router.push('/shipping')}
+                        type='button'
+                        className='rounded-xl bg-gray-300 text-gray-700 px-4 py-2 w-28'
+                    >
+                        Back
+                    </button>
+                    <button className='rounded-xl bg-gray-700 text-white px-4 py-2 w-28'>
+                        Next
+                    </button>
+                </div>
+            </form>
+        </Layout>
+    )
+}
+
+export default PaymentPage
