e195e85721383c44471d5018cc86d4011d77385e
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 03:36:11 +0330

    add SAVE_PAYMENT_METHOD

--- context/Cart.js
+++ context/Cart.js
@@ -36,6 +36,14 @@ function reducer(state, action) {
                     },
                 },
             }
+        case 'SAVE_PAYMENT_METHOD':
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    paymentMethod: action.payload,
+                },
+            }
         default:
             return state
     }
