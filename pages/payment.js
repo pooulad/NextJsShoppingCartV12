03f2b1200af010e9e0af1e7a329cc8206f129e01
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 04:56:32 +0330

    Update payment.js

--- pages/payment.js
+++ pages/payment.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import Cookies from 'js-cookie'
 import { CartContext } from '../context/Cart'
 import Layout from '../components/Layout'
@@ -8,33 +8,33 @@ import { toast } from 'react-toastify'
 
 function PaymentPage() {
     const { state, dispatch } = useContext(CartContext)
-
     const { cart } = state
     const { paymentMethod } = cart
-
     const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('')
-
     const router = useRouter()
-
+    useEffect(() => {
+        setSelectedPaymentMethod(paymentMethod)
+    }, [paymentMethod])
     function submitHandler(event) {
         event.preventDefault()
 
         if (!selectedPaymentMethod) {
             toast.error('Please Select Payment Method')
             return
+        } else {
+            dispatch({ type: ' SAVE_PAYMENT_METHOD', payload: selectedPaymentMethod })
+
+            Cookies.set(
+                'cart',
+                JSON.stringify({
+                    ...cart,
+                    paymentMethod: selectedPaymentMethod,
+                })
+            )
+
+            router.push('/placeorder')
         }
 
-        dispatch({ type: ' SAVE_PAYMENT_METHOD', payload: selectedPaymentMethod })
-
-        Cookies.set(
-            'cart',
-            JSON.stringify({
-                ...cart,
-                paymentMethod: selectedPaymentMethod,
-            })
-        )
-
-        router.push('/placeorder')
     }
 
     const methods = ['Gateway', 'Offline Payment']
