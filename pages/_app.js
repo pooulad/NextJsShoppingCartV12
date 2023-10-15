bf19f02f31ba4a53e49df866e6aa557aeb28f69b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 03:58:26 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,23 @@
-import { CartContextProvider } from '../context/Cart'
 import '../styles/globals.css'
-import { SessionProvider, useSession } from "next-auth/react"
+
+import { SessionProvider, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+
+import { StoreProvider } from '../context/Cart'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <div className='bg-gray-100'>
       <SessionProvider session={session}>
-        <CartContextProvider>
-          <Auth adminOnly={Component.auth.adminOnly}>
+        <StoreProvider>
+          {Component.auth ? (
+            <Auth adminOnly={Component.auth.adminOnly}>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
             <Component {...pageProps} />
-          </Auth>
-        </CartContextProvider>
+          )}
+        </StoreProvider>
       </SessionProvider>
     </div>
   )
@@ -19,6 +25,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 
 function Auth({ children, adminOnly }) {
   const router = useRouter()
+
   const { status, data: session } = useSession({
     required: true,
     onUnauthenticated() {
@@ -35,7 +42,6 @@ function Auth({ children, adminOnly }) {
   }
 
   return children
-
 }
 
 export default MyApp
