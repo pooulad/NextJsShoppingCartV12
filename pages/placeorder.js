7d6dd82dd048472231ebb25db61905c566f6f582
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-15 04:24:58 +0330

    Create placeorder.js

---
+++ pages/placeorder.js
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { useContext } from 'react'
+import { CartContext } from '../context/Cart'
+import Layout from '../components/Layout'
+import CheckoutWizard from '../components/CheckoutWizard'
