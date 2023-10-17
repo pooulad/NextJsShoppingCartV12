8420654d1e5d105d9a3eeb1ee7264cfa1bd306a0
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-18 02:57:42 +0330

    Update Cart.js

--- context/Cart.js
+++ context/Cart.js
@@ -1,75 +1,59 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer } from "react";
+import Cookies from "js-cookie";
 
-import Cookies from 'js-cookie'
 
-export const Store = createContext()
+export const CartContext = createContext();
 
 const initialState = {
-  cart: Cookies.get('cart')
-    ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [], shippingData: {} },
+    cart: Cookies.get("cart") ? JSON.parse(Cookies.get("cart")) : { cartItems: [], shippingData: {} }
 }
+
 
 function reducer(state, action) {
-  switch (action.type) {
-    case 'ADD_TO_CART': {
-      const newItem = action.payload
+    switch (action.type) {
+        case "ADD_TO_CART": {
+            const newItem = action.payload
+            const existingItem = state.cart.cartItems.find((item) => item.slug === newItem.slug)
+            const cartItems = existingItem ? state.cart.cartItems.map((item) => item.title === existingItem.title ? newItem : item) : [...state.cart.cartItems, newItem]
 
-      const existingItem = state.cart.cartItems.find(
-        (item) => item.slug === newItem.slug
-      )
+            Cookies.set("cart", JSON.stringify({ ...state.cart, cartItems }))
+            return { ...state, cart: { ...state.cart, cartItems } }
+        }
+        case "REMOVE_ITEM": {
+            const cartItems = state.cart.cartItems.filter((item) => item.slug !== action.payload.slug)
 
-      const cartItems = existingItem
-        ? state.cart.cartItems.map((item) =>
-            item.title === existingItem.title ? newItem : item
-          )
-        : [...state.cart.cartItems, newItem]
-
-      Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }))
-
-      return { ...state, cart: { ...state.cart, cartItems } }
+            Cookies.set("cart", JSON.stringify({ ...state.cart, cartItems }))
+            return { ...state, cart: { ...state.cart, cartItems } }
+        }
+        case 'SAVE_SHIPPING_DATA':
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    shippingData: {
+                        ...state.cart.shippingData,
+                        ...action.payload,
+                    },
+                },
+            }
+        case 'SAVE_PAYMENT_METHOD':
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    paymentMethod: action.payload,
+                },
+            }
+        default:
+            return state
     }
-
-    case 'REMOVE_ITEM': {
-      const cartItems = state.cart.cartItems.filter(
-        (item) => item.slug !== action.payload.slug
-      )
-
-      Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }))
-
-      return { ...state, cart: { ...state.cart, cartItems } }
-    }
-
-    case 'SAVE_SHIPPING_DATA':
-      return {
-        ...state,
-        cart: {
-          ...state.cart,
-          shippingData: {
-            ...state.cart.shippingData,
-            ...action.payload,
-          },
-        },
-      }
-
-    case 'SAVE_PAYMENT_METHOD':
-      return {
-        ...state,
-        cart: {
-          ...state.cart,
-          paymentMethod: action.payload,
-        },
-      }
-
-    default:
-      return state
-  }
 }
 
-export function StoreProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState)
+export function CartContextProvider({ children }) {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const value = { state, dispatch }
 
-  const value = { state, dispatch }
-
-  return <Store.Provider value={value}>{children}</Store.Provider>
+    return <CartContext.Provider value={value}>
+        {children}
+    </CartContext.Provider>
 }
