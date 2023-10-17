ed2ca1bd789f201850ac8f04fa1fa515b6d3328b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-18 02:55:21 +0330

    Create index.js

---
+++ pages/api/orders/index.js
@@ -0,0 +1,27 @@
+import { getSession } from 'next-auth/react'
+
+import db from '../../../utils/db'
+import Order from '../../../models/order'
+
+async function handler(req, res) {
+  const session = await getSession({ req })
+
+  if (!session) {
+    return res.send('Sing in required')
+  }
+
+  const { user } = session
+
+  await db.connect()
+
+  const newOrder = new Order({
+    ...req.body,
+    user: user._id,
+  })
+
+  const order = await newOrder.save()
+
+  res.status(201).send(order)
+}
+
+export default handler
