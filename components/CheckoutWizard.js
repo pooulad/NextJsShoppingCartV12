8caad672930ac15759f02a8315585c63f7910ee4
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-18 02:56:43 +0330

    Update CheckoutWizard.js

--- components/CheckoutWizard.js
+++ components/CheckoutWizard.js
@@ -1,24 +1,21 @@
-function CheckoutWizard({ activeStep = 0 }) {
-  const titles = ['User Login', 'Address', 'Payment Method', 'Place Order']
+import React from 'react'
 
-  return (
-    <div className='mb-5 flex flex-wrap'>
-      {titles.map((item, index) => (
-        <div
-          className={`flex-1 border-b-2 text-center
-          ${
-            index <= activeStep
-              ? 'border-blue-600 text-blue-600'
-              : 'border-gray-400 text-gray-400'
-          }
-          `}
-          key={item}
-        >
-          {item}
+function CheckoutWizard({ activeStep = 0 }) {
+    const titles = [
+        "User Login",
+        "Address",
+        "Payment Method",
+        "Place Order",
+    ]
+    return (
+        <div className='mb-5 flex flex-wrap'>
+            {titles.map((item, index) => (
+                <div className={
+                    `flex-1 border-b-2 text-center ${index <= activeStep ? "border-blue-600 text-blue-600" : "border-gray-400 text-gray-400"}`
+                } key={item}>{item}</div>
+            ))}
         </div>
-      ))}
-    </div>
-  )
+    )
 }
 
 export default CheckoutWizard
