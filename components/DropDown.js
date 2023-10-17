6b9fd81830fbab7cb65ce5b9c44dd6d81824a577
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-18 02:54:17 +0330

    Update DropDown.js

--- components/DropDown.js
+++ components/DropDown.js
@@ -1,11 +1,11 @@
-import Link from "next/link"
+import Link from 'next/link'
 
 function DropDown({ href, children, ...rest }) {
-    return (
-        <Link href={href}>
-            <a {...rest}>{children}</a>
-        </Link>
-    )
+  return (
+    <Link href={href}>
+      <a {...rest}>{children}</a>
+    </Link>
+  )
 }
 
 export default DropDown
