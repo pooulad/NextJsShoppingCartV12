676331cd0c59118b1ca7b8588b148314b156951b
pooulad <86445458+pooulad@users.noreply.github.com>
2023-10-18 02:54:27 +0330

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,23 +1,22 @@
 const products = [
   {
-    title: "book 1",
-    price: 300,
-    slug: "book-1",
-    description: "test description",
-    cat: "books",
-    cound: 10,
-    image: "/images/book.jpg"
+    title: 'Book 1',
+    price: 299,
+    slug: 'book-1',
+    description: 'New Book',
+    cat: 'Books',
+    count: 2,
+    image: '/images/book.jpg',
   },
   {
-    title: "book 2",
-    price: 200,
-    slug: "book-2",
-    description: "test description",
-    cat: "books",
-    cound: 10,
-    image: "/images/book.jpg"
-  }
+    title: 'Book 2',
+    price: 99,
+    slug: 'book-2',
+    description: 'New Book',
+    cat: 'Books',
+    count: 5,
+    image: '/images/book.jpg',
+  },
 ]
-
 
 export default products
